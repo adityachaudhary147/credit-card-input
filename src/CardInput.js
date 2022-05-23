bbb2a7554a10f4547dbdba0320b84361e45b174c
aditya chaudhary <chaudharyaditya.in@gmail.com>
2022-05-23 12:31:40 +0530

    input design created

---
+++ src/CardInput.js
@@ -0,0 +1,78 @@
+
+import { useState } from 'react';
+import './CardInput.css';
+function isNumber(char) {
+    return /^\d$/.test(char);
+  }
+export const CardInput=()=>{
+    
+    const [state,setState]=useState({cardNumber:""});
+
+    function operateCardInput(e){
+
+        // e.preventDefault();
+        
+        const val=e.target.value;
+        const len=val.length;
+        
+        if(len== 4 || len==9 || len== 14)
+        {
+            setState({cardNumber:val+'-'});
+        }
+        else{    
+            if(isNumber(val[len-1])==true)
+            {
+                setState({cardNumber:val});
+              
+
+                return;
+            }
+            else{
+                
+                setState({cardNumber:val.slice(0,-1)})
+                
+                return;
+            }
+        }
+      
+    }
+    function captureDelete(e)
+    {
+        // e.preventDefault();
+       
+        const curr=state.cardNumber.length;
+        if(curr ===5 || curr === 10|| curr ===15 )
+            {
+                if(e.keyCode==8)
+                {
+                setState((val)=>{
+                    return {cardNumber:val.cardNumber.slice(0,-1)}
+                });
+            }
+        }
+        return;
+    }
+
+    const last4digit=[3,4,5,6];
+    return (
+        <div>
+
+            <div className="card-box">
+                <h1> Enter Credit Card Number</h1>
+
+                <div className="container-input">
+
+                    <div>
+                    {/*  */}
+                        <input type='string' maxLength='14' className="card-input-box" value={state.cardNumber} placeholder='XXXX-XXXX-XXXX-' onChange={(e)=>{operateCardInput(e)}} onKeyDown={(e)=>{captureDelete(e)}} ></input>
+                    </div>
+                    <div className="last-fout-digit">
+                        <div>{last4digit.map(val=>{return <span className='card-digits'>{val}</span>})}</div>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    )
+}
