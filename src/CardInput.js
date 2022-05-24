7728a4af118278497b927a77aecdb9f6a19e288c
aditya chaudhary <chaudharyaditya.in@gmail.com>
2022-05-24 09:27:52 +0530

    verify card number

--- src/CardInput.js
+++ src/CardInput.js
@@ -1,87 +1,71 @@
-
-import { useState } from 'react';
-import './CardInput.css';
+import { useState } from "react";
+import "./CardInput.css";
 function isNumber(char) {
-    return /^\d$/.test(char);
-  }
-export const CardInput=()=>{
-    
-    const [state,setState]=useState({cardNumber:""});
-
-    function operateCardInput(e){
-
-        // e.preventDefault();
-        
-        const val=e.target.value;
-        const len=val.length;
-
-        
-        if(isNumber(val[len-1])==true && (len== 4 || len==9 || len== 14))
-        {
-            setState({cardNumber:val+'-'});
-        }
-        else{    
-            if(isNumber(val[len-1])==true)
-            {
-                setState({cardNumber:val});
-              
-
-                return;
-            }
-            else{
-                if(len==5 || len==10 || len==15)
-                {
-                    setState({cardNumber:val});
-                return;
-                }
-                setState({cardNumber:val.slice(0,-1)})
-                
-                return;
-            }
-        }
-      
-    }
-    function captureDelete(e)
-    {
-        
-       
-        const curr=state.cardNumber.length;
-        
-       
-        if(curr ===5 || curr === 10|| curr ===15 )
-            {
-                console.log(curr);
-                if(e.keyCode==8)
-                {
-                    // e.preventDefault();
-                setState((val)=>{
-                    return {cardNumber:val.cardNumber.slice(0,-1)}
-                });
-            }
-        }
-        return;
-    }
-
-    const last4digit=[3,4,5,6];
-    return (
-        <div>
-
-            <div className="card-box">
-                <h1> Enter Credit Card Number</h1>
-
-                <div className="container-input">
-
-                    <div>
-                    {/*  */}
-                        <input type='string' maxLength='14' className="card-input-box" value={state.cardNumber} placeholder='XXXX-XXXX-XXXX-' onChange={(e)=>{operateCardInput(e)}} onKeyDown={(e)=>{captureDelete(e)}} ></input>
-                    </div>
-                    <div className="last-fout-digit">
-                        <div>{last4digit.map(val=>{return <span className='card-digits'>{val}</span>})}</div>
-                    </div>
-
-                </div>
-
-            </div>
-        </div>
-    )
+  return /^\d$/.test(char);
 }
+function operateval(val) {
+  const rawText = [...val.split("-").join("")];
+  const creditCard = [];
+  rawText.forEach((t, i) => {
+    if (i % 4 === 0) {
+      if (i != 0) creditCard.push("-");
+    }
+    creditCard.push(t);
+  });
+  return creditCard.join("");
+}
+
+
+export const CardInput = () => {
+  const [state, setState] = useState({ cardNumber: "" });
+
+  function operateCardInput(e) {
+    var val = e.target.value;
+    var len = val.length;
+    var we = operateval(val);
+    var val = we;
+    if (isNumber(val[len - 1]) == true) {
+      setState({ cardNumber: val });
+    } else {
+      if (len == 5 || len == 10 || len == 15) {
+        setState({ cardNumber: val });
+        
+      } else {
+        setState({ cardNumber: val.slice(0, -1) });
+      }
+    }
+    return;
+  }
+
+  const last4digit = ["-", 3, 4, 5, 6];
+  return (
+    <div>
+      <div className="card-box">
+        <h1> Verify Your Credit Card Number</h1>
+
+        <div className="container-input">
+          <div>
+            {/*  */}
+            <input
+              type="string"
+              maxLength="14"
+              className="card-input-box"
+              value={state.cardNumber}
+              placeholder="XXXX-XXXX-XXXX"
+              onChange={(e) => {
+                operateCardInput(e);
+              }}
+            ></input>
+          </div>
+          <div className="last-fout-digit">
+            <div>
+              {last4digit.map((val) => {
+                return <span className="card-digits">{val}</span>;
+              })}
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
